b4e339025761f1cda83bf26331b539f71397f250
RomeZhur <roman.edyardovich@gmail.com>
2022-04-21 19:57:21 +0300

    00_lesson

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {finished} from "stream";
 
 ReactDOM.render(
     <App />,
@@ -12,7 +13,11 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+
+
+
 
 // let a = {
 //     name: "Roman",
@@ -23,35 +28,50 @@ serviceWorker.unregister();
 //     }
 // };
 
-let users = [
-    {
-        name: "Roman",
-        age: 30,
-        address: {
-            city: "Minsk",
-            country: "Belarus"
-        }
-    },
-    {
-        name: "Alex",
-        age: 20,
-        address: {
-            city: "Minsk",
-            country: "Belarus"
-        }
-    },
-    {
-        name: "Oleg",
-        age: 40,
-        address: {
-            city: "Minsk",
-            country: "Belarus"
-        }
-    }
-]
+// let users = [
+//     {
+//         name: "Roman",
+//         age: 30,
+//         address: {
+//             city: "Minsk",
+//             country: "Belarus"
+//         }
+//     },
+//     {
+//         name: "Alex",
+//         age: 20,
+//         address: {
+//             city: "Minsk",
+//             country: "Belarus"
+//         }
+//     },
+//     {
+//         name: "Oleg",
+//         age: 40,
+//         address: {
+//             city: "Minsk",
+//             country: "Belarus"
+//         }
+//     }
+// ]
+//
+// let b = 10;
+// b     = 115;
+//
+// // console.log (a.address.country);
+// console.log (users[0].address.country);
 
-let b = 10;
-b     = 115;
 
-// console.log (a.address.country);
-console.log (users[0].address.country);
+function App () {
+    // полезное здесь
+    // возвращаем JSX => HTML подобный js
+    return  (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+};
