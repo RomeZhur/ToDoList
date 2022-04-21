9ecd8f3a35ec02b87c676e30355eeb153ad17da2
RomeZhur <roman.edyardovich@gmail.com>
2022-04-21 19:25:01 +0300

    00_lesson

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+let a = {
+    name: "Roman",
+    age: 30,
+    address: {
+        city: "Minsk",
+        country: "Belarus"
+    }
+};
+
+console.log (a.address.country);
+
