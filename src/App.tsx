2606e3cc4a519d751a3482cfbf1d8ff7811a29e6
RomeZhur <roman.edyardovich@gmail.com>
2022-04-21 21:01:06 +0300

    00_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './App.css';
 
-// function App() {
-//     return (
-//         <div className="App">
-//             <div>
-//                 <h3>What to learn</h3>
-//                 <div>
-//                     <input/>
-//                     <button>+</button>
-//                 </div>
-//                 <ul>
-//                     <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-//                     <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-//                     <li><input type="checkbox" checked={false}/> <span>React</span></li>
-//                 </ul>
-//                 <div>
-//                     <button>All</button>
-//                     <button>Active</button>
-//                     <button>Completed</button>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 
 // let a = {
@@ -68,51 +68,51 @@ import './App.css';
 // // console.log (a.address.country);
 // console.log (users[0].address.country);
 
-function App () {
-    // полезное здесь
-    // возвращаем JSX => HTML подобный js
-    return  (
-        <div>
-            This is APP
-            <Rating/>
-            <Accordion/>
-        </div>
-    );
-}
-
-function Rating () {
-    // полезное здесь
-    // возвращаем JSX => HTML подобный js
-    return  (
-        <div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-            <div>star</div>
-        </div>
-    );
-}
-
-function Accordion () {
-    // полезное здесь
-    // возвращаем JSX => HTML подобный js
-    return <div>
-        <h3>Меню</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </div>
-}
-
-
-// function Hello () {
-//     debugger;
-//     alert("Hello")
+// function App () {
+//     // полезное здесь
+//     // возвращаем JSX => HTML подобный js
+//     return  (
+//         <div>
+//             This is APP
+//             <Rating/>
+//             <Accordion/>
+//         </div>
+//     );
 // }
 //
-// Hello();
+// function Rating () {
+//     // полезное здесь
+//     // возвращаем JSX => HTML подобный js
+//     return  (
+//         <div>
+//             <div>star</div>
+//             <div>star</div>
+//             <div>star</div>
+//             <div>star</div>
+//             <div>star</div>
+//         </div>
+//     );
+// }
+//
+// function Accordion () {
+//     // полезное здесь
+//     // возвращаем JSX => HTML подобный js
+//     return <div>
+//         <h3>Меню</h3>
+//         <ul>
+//             <li>1</li>
+//             <li>2</li>
+//             <li>3</li>
+//         </ul>
+//     </div>
+// }
+//
+//
+// // function Hello () {
+// //     debugger;
+// //     alert("Hello")
+// // }
+// //
+// // Hello();
 
 export default App;
